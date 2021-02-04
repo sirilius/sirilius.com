d17f339c2043fd725d3b094a57426e1202a76311
Sirilius Kevin <siriliuskevin@gmail.com>
2021-02-04 20:34:51 +0700

    add copyright

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -64,7 +64,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     <footer className="site-foot">
                         <div className="site-foot-nav container">
                             <div className="site-foot-nav-left">
-                                <Link to="/">{site.title}</Link> © 2021 &mdash; Published with <a className="site-foot-nav-item" href="https://ghost.org" target="_blank" rel="noopener noreferrer">Ghost</a>
+                                &copy; <script>document.write(new Date().getFullYear());</script> Sirilius Kevin
                             </div>
                             <div className="site-foot-nav-right">
                                 <Navigation data={site.navigation} navClass="site-foot-nav-item" />
